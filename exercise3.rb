0f415b26440f35ec2961ef64b56e7ce675c6d6ae
Jonathan Alorda <jonathan.alorda@gmail.com>
2016-01-12 16:12:34 -0500

    Commit of exercise3.rb with some extra features.

--- exercise3.rb
+++ exercise3.rb
@@ -2,6 +2,18 @@
 
 puts "What is your name?"
 
-name = gets.strip
+name = gets.chomp
 
-put "Hi #{name}!"
+puts "Hi #{name}! How old are you? I'm going to guess your birth year. I can only guarantee general accuracy since we aren't dealing with months and days at this point..."
+
+age = gets.chomp.to_i
+
+puts "I'm guessing you were born during or pretty close to the year #{2016 - age}?\n (Answer 'yes' or 'no')"
+
+yes_no = gets.chomp.downcase
+
+if yes_no == "yes"
+  puts "I was correct!"
+else
+  puts "I was wrong. Sad face. In binary."
+end
