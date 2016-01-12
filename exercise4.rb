4b3e08276d2bb68b51a8aadedfcab7fa2379abb6
Jonathan Alorda <jonathan.alorda@gmail.com>
2016-01-12 16:32:21 -0500

    Commit of exercise4.rb ** added iteration to achieve same goal.

--- exercise4.rb
+++ exercise4.rb
@@ -11,3 +11,22 @@ for num in (1..100) do
     puts num
   end
 end
+
+#We can also iterate to achieve the same goal:
+puts "We can also iterate to achieve the same goal:\n"
+
+
+
+one_to_a_hundred = (1..100)
+
+one_to_a_hundred.each do |num|
+  if num % 3 == 0 && num % 5 == 0
+    puts "BitMaker"
+  elsif num % 3 == 0
+    puts "Bit"
+  elsif num % 5 == 0
+    puts "Maker"
+  else
+    puts num
+  end
+end 
